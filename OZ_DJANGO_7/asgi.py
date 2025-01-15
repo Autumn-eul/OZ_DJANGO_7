542350a09d1338fb8260eca3475a6a35770e9dc1
BlueLight <dlrkdmf10@gmail.com>
2025-01-15 11:45:22 +0900

    run black

--- OZ_DJANGO_7/asgi.py
+++ OZ_DJANGO_7/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OZ_DJANGO_7.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OZ_DJANGO_7.settings")
 
 application = get_asgi_application()
