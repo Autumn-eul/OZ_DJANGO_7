28432108616e16212fb5ab12e17c3927ae603c92
BlueLight <dlrkdmf10@gmail.com>
2025-01-15 14:18:51 +0900

    run mypy

--- OZ_DJANGO_7/settings.py
+++ OZ_DJANGO_7/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5itwvaaa9&sq@3iw53knownru=410!9%72@to%v)m1=n^rwtr%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
