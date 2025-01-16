e2309becead85f0745b4efa6a3630f1ebe042c34
BlueLight <dlrkdmf10@gmail.com>
2025-01-16 11:01:12 +0900

    fix ci

--- OZ_DJANGO_7/settings.py
+++ OZ_DJANGO_7/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import pymysql
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,10 +75,14 @@ WSGI_APPLICATION = "OZ_DJANGO_7.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+pymysql.install_as_MySQLdb()
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "oz_django_7",
+        "USER": "root",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
